89b1690a032471c4f2f4a2703408260f5bbffaf6
syarifuddin.sadikin@gmail.com <syarifuddin.sadikin@gmail.com>
2015-06-02 06:30:08 +1000

    pass first parsing test

--- src/main/java/com/sen/papercut/PrintJobParser.java
+++ src/main/java/com/sen/papercut/PrintJobParser.java
@@ -1,13 +1,31 @@
 package com.sen.papercut;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
 /**
  * Created by sen on 2/06/2015.
  */
 public class PrintJobParser {
-    public static List<PrintJobItem> parse(String inputLine) {
-        return new ArrayList<PrintJobItem>();
+
+    public static Iterable<PrintJobItem> parse(String line) {
+        String[] s = line.split(", ");
+
+        int totalPages = Integer.parseInt(s[0]);
+        int colorPages = Integer.parseInt(s[1]);
+        boolean doubleSided = Boolean.parseBoolean(s[2]);
+
+        PrintType printType = doubleSided ? PrintType.DOUBLESIDED : PrintType.SINGLESIDED;
+        int bwPages = totalPages - colorPages;
+
+        return createJobItems(colorPages, printType, bwPages);
+    }
+
+    private static Iterable<PrintJobItem> createJobItems(int colorPages, PrintType printType, int bwPages) {
+        List<PrintJobItem> jobs = new ArrayList<>(2);
+        PrintJobItem colorJob = new PrintJobItem(ColorType.COLOR, colorPages, printType);
+        PrintJobItem bwJob = new PrintJobItem(ColorType.BLACKWHITE, bwPages, printType);
+
+        return Arrays.asList(colorJob, bwJob);
     }
 }
--- src/test/java/com/sen/papercut/PrintJobParserTest.java
+++ src/test/java/com/sen/papercut/PrintJobParserTest.java
@@ -1,6 +1,6 @@
 package com.sen.papercut;
 
-import java.util.List;
+import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -8,9 +8,10 @@ import static junit.framework.Assert.assertEquals;
  * Created by sen on 2/06/2015.
  */
 public class PrintJobParserTest {
+    @Test
     public void parseSingleSided() {
         String input = "25, 10, false";
-        List<PrintJobItem> jobItems = PrintJobParser.parse(input);
+        Iterable<PrintJobItem> jobItems = PrintJobParser.parse(input);
         for (PrintJobItem jobItem : jobItems) {
             assertEquals("all job items must be single sided", PrintType.SINGLESIDED, jobItem.getPrintType());
         }
