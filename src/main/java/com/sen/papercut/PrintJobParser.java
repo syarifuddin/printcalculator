2d1bc1be1dd98153a29ce234d53eb62fda029ad4
syarifuddin.sadikin@gmail.com <syarifuddin.sadikin@gmail.com>
2015-06-02 06:49:40 +1000

    refactor to use java stream api

--- src/main/java/com/sen/papercut/PrintJobParser.java
+++ src/main/java/com/sen/papercut/PrintJobParser.java
@@ -1,14 +1,15 @@
 package com.sen.papercut;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+
 /**
  * Created by sen on 2/06/2015.
  */
 public class PrintJobParser {
 
-    public static Iterable<PrintJobItem> parse(String line) {
+    public static Collection<PrintJobItem> parse(String line) {
         String[] s = line.split(", ");
 
         int totalPages = Integer.parseInt(s[0]);
@@ -21,11 +22,13 @@ public class PrintJobParser {
         return createJobItems(colorPages, printType, bwPages);
     }
 
-    private static Iterable<PrintJobItem> createJobItems(int colorPages, PrintType printType, int bwPages) {
-        List<PrintJobItem> jobs = new ArrayList<>(2);
+    private static Collection<PrintJobItem> createJobItems(int colorPages, PrintType printType, int bwPages) {
+
         PrintJobItem colorJob = new PrintJobItem(ColorType.COLOR, colorPages, printType);
         PrintJobItem bwJob = new PrintJobItem(ColorType.BLACKWHITE, bwPages, printType);
-
-        return Arrays.asList(colorJob, bwJob);
+        List<PrintJobItem> jobs = new ArrayList<>(2);
+        jobs.add(colorJob);
+        jobs.add(bwJob);
+        return jobs;
     }
 }
--- src/test/java/com/sen/papercut/PrintJobParserTest.java
+++ src/test/java/com/sen/papercut/PrintJobParserTest.java
@@ -2,7 +2,9 @@ package com.sen.papercut;
 
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
+import java.util.Collection;
+
+import static junit.framework.Assert.assertTrue;
 
 /**
  * Created by sen on 2/06/2015.
@@ -11,9 +13,27 @@ public class PrintJobParserTest {
     @Test
     public void parseSingleSided() {
         String input = "25, 10, false";
-        Iterable<PrintJobItem> jobItems = PrintJobParser.parse(input);
-        for (PrintJobItem jobItem : jobItems) {
-            assertEquals("all job items must be single sided", PrintType.SINGLESIDED, jobItem.getPrintType());
-        }
+        Collection<PrintJobItem> jobItems = PrintJobParser.parse(input);
+        boolean allSingleSided = jobItems.stream().allMatch(j -> j.getPrintType() == PrintType.SINGLESIDED);
+        assertTrue("all job items must be single sided", allSingleSided);
+    }
+
+    @Test
+    public void parseDoubleSidedJobs() {
+        String input = "502, 22, true";
+        Collection<PrintJobItem> jobItems = PrintJobParser.parse(input);
+        boolean allDoubleSided = jobItems.stream().allMatch(j -> j.getPrintType() == PrintType.DOUBLESIDED);
+        assertTrue("all job items must be double sided", allDoubleSided);
+    }
+
+    @Test
+    public void parseReturnColorAndBlackWhiteJobs() {
+        String input = "25, 10, false";
+        Collection<PrintJobItem> jobItems = PrintJobParser.parse(input);
+        boolean hasBWJob = jobItems.stream().anyMatch(j -> j.getColorType() == ColorType.BLACKWHITE);
+        assertTrue("must contain black and white job", hasBWJob);
+        boolean hasColorJob = jobItems.stream().anyMatch(j -> j.getColorType() == ColorType.BLACKWHITE);
+        assertTrue("must contain color job", hasBWJob);
+
     }
 }
