d53c32ece0cf78abec5f2005a5a2cc88b1f4058a
syarifuddin.sadikin@gmail.com <syarifuddin.sadikin@gmail.com>
2015-06-04 01:21:03 +1000

    tests for optional totalcost value

--- src/test/java/com/sen/papercut/PrintJobCostCalculatorTest.java
+++ src/test/java/com/sen/papercut/PrintJobCostCalculatorTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import java.math.BigDecimal;
 import java.util.function.Function;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Created by sen on 3/06/2015.
@@ -25,5 +25,25 @@ public class PrintJobCostCalculatorTest {
         PrintJobItem result = job.calculateCost(costFinder);
 
         assertEquals("Expected cost doesn't match", expectedCost, result.getTotalCost().get());
+    }
+
+    @Test
+    public void getTotalCost_ReturnsEmpty_IfNot_Calculated() {
+        int numberOfPages = 10;
+        PrintJobItem job = new PrintJobItem(aPrintingSpec, numberOfPages);
+        assertFalse("total Must return empty before job is calculated", job.getTotalCost().isPresent());
+    }
+
+    @Test
+    public void getTotalCost_ReturnsNonEmpty_If_Calculated() {
+        int numberOfPages = 10;
+        BigDecimal unitCost = new BigDecimal(20);
+
+        Function<PrintingSpec, BigDecimal> costFinder = s -> unitCost;
+        PrintJobItem job = new PrintJobItem(aPrintingSpec, numberOfPages);
+
+        PrintJobItem result = job.calculateCost(costFinder);
+
+        assertTrue("total Must return non empty after job is calculated", result.getTotalCost().isPresent());
     }
 }
