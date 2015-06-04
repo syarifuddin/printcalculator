ecfe454c816ad7726ebd568583bec2df70bea99a
syarifuddin.sadikin@gmail.com <syarifuddin.sadikin@gmail.com>
2015-06-04 22:59:35 +1000

    relayout output

--- src/main/java/com/sen/papercut/App.java
+++ src/main/java/com/sen/papercut/App.java
@@ -1,16 +1,11 @@
 package com.sen.papercut;
 
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.NumberFormat;
 import java.util.List;
-import java.util.Locale;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Hello world!
@@ -18,31 +13,21 @@ import java.util.stream.Stream;
  */
 public class App 
 {
-    private static final NumberFormat money;
-
-    static {
-        money = NumberFormat.getCurrencyInstance(Locale.CANADA);
-        money.setRoundingMode(RoundingMode.HALF_EVEN);
-    }
-
     public static void main( String[] args )
     {
         Path path = Paths.get("/Users/sen/Dev/printcalculator", "printjobs.csv");
 
         try {
-            List<PrintJob> jobList = readPrintJobFrom(path).collect(Collectors.toList());
             PrintJobFormatter.PrintJobs(readPrintJobFrom(path)).forEach(System.out::println);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static String moneyFormat(BigDecimal price) {
-        return money.format(price.setScale(2, RoundingMode.HALF_EVEN));
-    }
+    private static List<PrintJob> readPrintJobFrom(Path filePath) throws IOException {
 
-    private static Stream<PrintJob> readPrintJobFrom(Path filePath) throws IOException {
-
-        return Files.lines(filePath).map(PrintJobParser::parseToPrintJob).map(j -> j.calculateCosts(PrintingCosts::getUnitCostForSpec));
+        return Files.lines(filePath).map(PrintJobParser::parseToPrintJob)
+                .map(j -> j.calculateCosts(PrintingCosts::getUnitCostForSpec))
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/sen/papercut/PrintJobFormatter.java
+++ src/main/java/com/sen/papercut/PrintJobFormatter.java
@@ -1,6 +1,8 @@
 package com.sen.papercut;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static java.util.stream.Stream.concat;
@@ -11,23 +13,36 @@ import static java.util.stream.Stream.concat;
 public class PrintJobFormatter {
     public static String PrintHeader() {
         return "===============================================================\n" +
-                " Page Size | Color | Double Sided | Pages |   Cost    \n" +
+                " Page Size |  Color  | Double Sided | Pages |   Cost    \n" +
                 "===============================================================";
 
     }
 
-    public static Stream<String> PrintJobs(Stream<PrintJob> jobs) {
-        return concat(Stream.of(PrintHeader()),
-                jobs.flatMap(PrintJobFormatter::PrintJob)
-        );
+    public static Stream<String> PrintJobs(List<PrintJob> jobs) {
+        return concat(
+                concat(Stream.of(PrintHeader()),
+                        jobs.stream().flatMap(PrintJobFormatter::PrintJob)),
+                PrintJobsTotalCosts(jobs));
+    }
+
+    private static Stream<String> PrintJobsTotalCosts(List<PrintJob> jobs) {
+
+        BigDecimal allJobTotalCosts = jobs.stream().map(PrintJob::getTotalCosts).reduce(new BigDecimal(0), (a, b) -> a.add(b));
+        return Stream.of(
+                String.format(
+                        "                                                ================\n" +
+                                "                            Jobs Total Cost:      $ %(,.2f      \n" +
+                                "                                                ================"
+                        ,
+                        allJobTotalCosts));
     }
 
     public static Stream<String> PrintJobTotalCost(PrintJob job) {
         return Stream.of(String.format(
-                "----------------------------------------------------------------\n" +
-                        " Job Total Cost                               $ %(,.2f          \n" +
-                        "----------------------------------------------------------------\n",
-                job.getTotalCosts()
+                "---------------------------------------------------------------\n" +
+                        "                         Job Sub Total Cost:    $ %(,.2f       \n" +
+                        "                                                ===============\n"
+                , job.getTotalCosts()
         ));
     }
 
@@ -38,11 +53,19 @@ public class PrintJobFormatter {
 
     public static String PrintJobItem(PrintJobItem item) {
         return String.format(
-                "    %s     | %b  |     %b     |  %3s  |   $ %(,.2f    ",
+                "    %s     |  %-5s  |     %s        |  %3s  |   $ %(,.2f    ",
                 item.getPrintingSpec().getPageSize(),
-                item.getPrintingSpec().getColorType(),
-                item.getPrintingSpec().getPrintType(),
+                formatColorType(item.getPrintingSpec().getColorType()),
+                formatPrintType(item.getPrintingSpec().getPrintType()),
                 item.getPages(),
                 item.getTotalCost().get());
+    }
+
+    private static String formatColorType(ColorType colorType) {
+        return ColorType.COLOR == colorType ? "Color" : "B/W";
+    }
+
+    private static String formatPrintType(PrintType printType) {
+        return PrintType.DOUBLESIDED == printType ? "Y" : "N";
     }
 }
